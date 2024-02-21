f4e0cd42a68c645104c444973a21195cbeb7b642
smallpip <96776159+smallpip@users.noreply.github.com>
2024-02-21 10:43:38 +0800

    Update server.py

--- src/server.py
+++ src/server.py
@@ -24,18 +24,20 @@ class Server:
         connected_clients (dict): A dictionary mapping client IDs to Client objects.
     """
     def __init__(self, vad_pipeline, asr_pipeline, host='localhost', port=8765, sampling_rate=16000, samples_width=2):
-        self.vad_pipeline = vad_pipeline
-        self.asr_pipeline = asr_pipeline
-        self.host = host
+        self.vad_pipeline = vad_pipeline # 配置 pyannote 识别管道
+        self.asr_pipeline = asr_pipeline # 配置 whisper 转录管道
+        self.host = host # 服务器地址、端口
         self.port = port
         self.sampling_rate = sampling_rate
         self.samples_width = samples_width
         self.connected_clients = {}
 
+    # 处理音频
     async def handle_audio(self, client, websocket):
         while True:
             message = await websocket.recv()
 
+            # 判断是否为音频字节还是配置文件
             if isinstance(message, bytes):
                 client.append_audio_data(message)
             elif isinstance(message, str):
@@ -47,9 +49,10 @@ class Server:
                 print(f"Unexpected message type from {client.client_id}")
 
             # this is synchronous, any async operation is in BufferingStrategy
+            # 处理音频、发送结果回请求端
             client.process_audio(websocket, self.vad_pipeline, self.asr_pipeline)
 
-
+    # 处理连接
     async def handle_websocket(self, websocket, path):
         client_id = str(uuid.uuid4())
         client = Client(client_id, self.sampling_rate, self.samples_width)
@@ -64,6 +67,7 @@ class Server:
         finally:
             del self.connected_clients[client_id]
 
+    # 启动服务
     def start(self):
         print("Websocket server ready to accept connections")
         return websockets.serve(self.handle_websocket, self.host, self.port)
